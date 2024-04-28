832a17e55fd608c082ebbf32becf24d6ce83942b
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-04-28 13:12:42 +0200

    task 1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,31 +1,31 @@
 
 export default function Profile({ name, tag, location, image, stats }) {
-    const userData = {
-username: "Jacques Gluke",
-        tag: "jgluke",
-        location: "Ocho Rios, Jamaica",
-        avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-        stats: {
- followers: 5603,
- views: 4827,
- likes: 1308
+    return (
+        <div>
+        <div>
+            <img
+              src={image}
+              alt="User avatar"
+            />
+            <p>{name}</p>
+            <p>@{tag}</p>
+            <p>{location}</p>
+        </div>
+        <ul>
+            <li>
+              <span>Followers</span>
+              <span>{stats.followers}</span>
+            </li>
+            <li>
+              <span>Views</span>
+              <span>{stats.views}</span>
+            </li>
+            <li>
+              <span>Likes</span>
+              <span>{stats.likes}</span>
+            </li>
+        </ul>
+        </div>
+    )
 }
-};
 
-
-const App = () => {
- return (
-<>
- <Profile
- name={userData.username}
-tag={userData.tag}
- location={userData.location}
- image={userData.avatar}
-stats={userData.stats}
- />
-</>
- );
-};
-   return <div></div>
-    
-}
