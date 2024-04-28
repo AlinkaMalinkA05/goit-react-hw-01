0cda3de54e311048e1f4b4836080aced195e22ec
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-04-28 21:55:31 +0200

    added some code in task 2

---
+++ src/components/Friendlist/Friendlist.jsx
@@ -0,0 +1,11 @@
+import FriendListItem from "../FriendsListItem/FriendListItem";
+export default function Friendlist({friends}) { 
+    
+    return (
+<ul>
+	<li>
+		<FriendListItem />
+	</li>
+</ul>
+    )
+}
