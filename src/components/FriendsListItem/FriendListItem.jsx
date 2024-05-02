75775fb65e7bfe9d164d6523f0a846bb20cd2892
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-05-02 22:25:13 +0200

    task 2

--- src/components/FriendsListItem/FriendListItem.jsx
+++ src/components/FriendsListItem/FriendListItem.jsx
@@ -1,8 +1,9 @@
-export default function FriendListItem({friend:{avatar, name, isOnline} }) {
+import css from "/src/components/FriendsListItem/FriendsListItem.module.css"
+export default function FriendListItem({ friend: { avatar, name, isOnline } }) {
     return (
-        <div>
+        <div className={css.wrapper}>
   <img src={avatar} alt="Avatar" width="48" />
-  <p>{name} </p>
+  <p className={css.friendsName} >{name} </p>
   <p>{isOnline ? "Online" : "Offline"}</p>
 </div>
 )
