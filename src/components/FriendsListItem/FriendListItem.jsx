b568c3e6b5fc8a9894244354d7fa72e8420ea337
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-05-04 22:30:17 +0200

    finished 3

--- src/components/FriendsListItem/FriendListItem.jsx
+++ src/components/FriendsListItem/FriendListItem.jsx
@@ -1,10 +1,11 @@
 import css from "/src/components/FriendsListItem/FriendsListItem.module.css"
+import clsx from "clsx"
 export default function FriendListItem({ friend: { avatar, name, isOnline } }) {
     return (
         <div className={css.wrapper}>
   <img src={avatar} alt="Avatar" width="48" />
   <p className={css.friendsName} >{name} </p>
-  <p>{isOnline ? "Online" : "Offline"}</p>
+  <p className={isOnline ? css.online : css.offline}>{isOnline ? "Online" : "Offline"}</p>
 </div>
 )
 }
